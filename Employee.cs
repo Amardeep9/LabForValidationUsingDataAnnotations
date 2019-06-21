dc373c162fa7f9c200a8b9af415053a4bccfe592
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-06-21 16:11:29 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabForValidationUsingDataAnnotations.Models
+{
+    public class Employee
+    {
+        [Required (ErrorMessage =" Please Enter UserName")]
+        [StringLength(30)]
+
+        [Display(Name = "Enter UserName")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = " Please Enter Email")]
+       // [RegularExpression(" .+@.+\\..+ ", ErrorMessage= "Email format is not good ")]
+
+          [Display(Name="Enter Mail")]
+        public string Email { get; set; }
+        [Range(20,35)]
+        [Required]
+        public string Age { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage ="Password and ConfirmPassword Should Match")]
+        public string ConfirmPassword { get; set; }
+        [ValidateCheckBox(ErrorMessage ="Please Check terms and Conditions")]
+        public bool Terms { get; set; }
+
+        public class ValidateCheckBox : RequiredAttribute
+        {
+            public override bool IsValid(object value)
+            {
+                return (bool)value;
+            }
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,51 @@
+﻿using LabForValidationUsingDataAnnotations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabForValidationUsingDataAnnotations.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee Emp)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Message");
+            }
+
+            return View();
+
+
+
+
+
+
+
+
+            //if (!string.IsNullOrEmpty(Emp.username))
+            //{
+            //    return RedirectToAction("Message");
+            //}
+            //else
+            //{
+            //    ModelState.AddModelError("username", "Username is Required");
+            //    return View();
+            //}
+        }
+
+        public ActionResult Message()
+        {
+            return View();
+        }
+    }
+}
